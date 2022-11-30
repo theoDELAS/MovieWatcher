b03b4c5e2e5efdc50a676009733ca935967839e4
Théo Delas <theo.delas@gmail.com>
2022-11-30 18:23:02 +0100

    Init MovierWatcher app

---
+++ app/src/main/java/fr/theodelas/moviematcher/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.theodelas.moviematcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
