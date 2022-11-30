01aa2cc333a5d4bf1f69f64c5e5d2346ca3d3f69
Théo Delas <theo.delas@gmail.com>
2022-11-30 21:18:40 +0100

    add bottom navigation for home and favs fragments

--- app/src/main/java/fr/theodelas/moviematcher/home/HomeActivity.kt
+++ app/src/main/java/fr/theodelas/moviematcher/home/HomeActivity.kt
@@ -2,11 +2,19 @@ package fr.theodelas.moviematcher.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import fr.theodelas.moviematcher.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
