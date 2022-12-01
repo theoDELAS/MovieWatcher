8cce8646d7b9cb31c6b2c99dc10b800f77f713d4
Théo Delas <theo.delas@gmail.com>
2022-12-01 12:03:22 +0100

    Fix lifecycle swiper. Move the swiper logic in the fragment instead the Activity

--- app/src/main/java/fr/theodelas/moviematcher/fragment/HomeFragment.kt
+++ app/src/main/java/fr/theodelas/moviematcher/fragment/HomeFragment.kt
@@ -5,7 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
+import androidx.lifecycle.ViewModelProvider
 import fr.theodelas.moviematcher.R
+import fr.theodelas.moviematcher.model.HomeModel
+import fr.theodelas.moviematcher.model.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,24 +26,54 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+
+        viewModel
+            .modelStream
+            .observe(viewLifecycleOwner, Observer {
+                bindCard(it)
+            })
+
+        motionLayout.setTransitionListener(object : TransitionAdapter() {
+            override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+                when (currentId) {
+                    R.id.offScreenUnlike,
+                    R.id.offScreenLike -> {
+                        motionLayout.progress = 0f
+                        motionLayout.setTransition(R.id.start, R.id.detail)
+                        viewModel.swipe()
+                    }
+                }
+            }
+        })
+
+        likeFloating.setOnClickListener {
+            motionLayout.transitionToState(R.id.like)
+        }
+
+        unlikeFloating.setOnClickListener {
+            motionLayout.transitionToState(R.id.unlike)
+        }
+    }
+
+    private fun bindCard(model: HomeModel) {
+        containerCardOne.setBackgroundColor(model.cardTop.backgroundColor)
+        name.text = "${model.cardTop.name}, ${model.cardTop.age}"
+        description.text = model.cardTop.description
+        containerCardTwo.setBackgroundColor(model.cardBottom.backgroundColor)
     }
 
     companion object {
